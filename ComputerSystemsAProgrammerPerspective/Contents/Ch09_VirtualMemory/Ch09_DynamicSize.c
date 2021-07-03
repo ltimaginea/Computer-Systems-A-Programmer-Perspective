d61378c82dfba50c26b3c0f9d5a31725a1690a84
ltimaginea <ltimaginea@163.com>
2021-07-03 23:52:19 +0800

    VirtualMemory

---
+++ ComputerSystemsAProgrammerPerspective/Contents/Ch09_VirtualMemory/Ch09_DynamicSize.c
@@ -0,0 +1,15 @@
+﻿#include "../code/include/csapp.h"
+
+int main()
+{
+	unsigned int n = 0;
+	scanf("%d", &n);
+	int* array = NULL;
+	array = (int*)Malloc(n * sizeof(int));
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%d", &array[i]);
+	}
+	Free(array);
+	return 0;
+}
