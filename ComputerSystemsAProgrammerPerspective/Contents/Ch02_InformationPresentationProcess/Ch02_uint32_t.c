805c2010bfbf1c04fe5febaa2e7c4b29d0ac39cb
ltimaginea <ltimaginea@163.com>
2021-07-16 12:56:23 +0800

    uint32_t

---
+++ ComputerSystemsAProgrammerPerspective/Contents/Ch02_InformationPresentationProcess/Ch02_uint32_t.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+int main()
+{
+	uint32_t sale = 0;
+	scanf("%" SCNu32, &sale);
+	printf("sale = %" PRIu32 "\n", sale);
+	printf("UINT32_MAX = %#" PRIx32 "\n", UINT32_MAX);
+	return 0;
+}
