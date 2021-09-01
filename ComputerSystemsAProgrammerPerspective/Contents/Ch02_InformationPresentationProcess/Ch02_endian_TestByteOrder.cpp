c5a735790c4b2ed8ce38cfff7752539fa07460e8
ltimaginea <ltimaginea@163.com>
2021-09-01 21:59:09 +0800

    TestByteOrder

--- ComputerSystemsAProgrammerPerspective/Contents/Ch02_InformationPresentationProcess/Ch02_endian_TestByteOrder.cpp
+++ ComputerSystemsAProgrammerPerspective/Contents/Ch02_InformationPresentationProcess/Ch02_endian_TestByteOrder.cpp
@@ -6,10 +6,9 @@ int main()
 {
 	int32_t a = 0x01234567;
 
-	int8_t* start_pointer = reinterpret_cast<int8_t*>(&a);
-	int32_t start_value = static_cast<int32_t>(*start_pointer);
+	int8_t start_value = *reinterpret_cast<int8_t*>(&a);
 
-	std::cout << std::showbase << std::hex << start_value << std::noshowbase << std::dec << "\n";
+	std::cout << std::showbase << std::hex << static_cast<int32_t>(start_value) << std::noshowbase << std::dec << "\n";
 	if (start_value == 0x67)
 		std::cout << "host is little-endian\n";
 	else if (start_value == 0x01)
