49744dc837d4fea54ce2bf343a4f5d876ac9fe76
ltimaginea <ltimaginea@163.com>
2021-09-02 15:57:03 +0800

    Endian Conversion Functions

---
+++ ComputerSystemsAProgrammerPerspective/Contents/Ch02_InformationPresentationProcess/Ch02_EndianConversionFunctions_Boost.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+#include <boost/endian/conversion.hpp>
+
+int main()
+{
+	using namespace boost::endian;
+
+	if (order::native == order::little)
+		std::cout << "native_order:\tlittle endian" << std::endl;
+	else if (order::native == order::big)
+		std::cout << "native_order:\tbig endian" << std::endl;
+	else
+		std::cout << "native_order:\tunknown endian" << std::endl;
+
+	int32_t data = 0x12345678;
+	std::cout << std::showbase << std::hex;
+	std::cout << "native_data:\t" << data << std::endl;
+	std::cout << "native_to_big:\t" << native_to_big(data) << std::endl;
+	std::cout << "native_to_lit:\t" << native_to_little(data) << std::endl;
+	std::cout << std::noshowbase << std::dec;
+	return 0;
+}
